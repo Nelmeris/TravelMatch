4063f50cca18a2ec1a9782b59078528e491c1348
Maxim Timokhin <m.v.timokhin@gmail.com>
2020-06-13 09:59:19 +0300

    Initial Commit

---
+++ Travel Match/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travel Match
+//
+//  Created by Maxim Timokhin on 13.06.2020.
+//  Copyright © 2020 Maxim Timokhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
