b206a4b7ac0200a46810ed4b254c329d0891dbc8
Artem Kufaev <kufaevartem@icloud.com>
2020-07-27 21:20:07 +0400

    Перераспределены модели по модулю сервисов

---
+++ Services/Services/LocalsServices/Models/LocalActivity.swift
@@ -0,0 +1,13 @@
+//
+//  LocalActivity.swift
+//  Services
+//
+//  Created by Artem Kufaev on 27.07.2020.
+//  Copyright © 2020 Artem Kufaev. All rights reserved.
+//
+
+import Locals
+
+struct LocalActivity: Activity {
+    let title: String
+}
